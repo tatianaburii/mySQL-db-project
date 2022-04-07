91d375577d67da5988d7ef322ac622b7e6372371
tatianaburii <azazazazazaz3333@gmail.com>
2022-04-07 14:54:26 +0300

    add method addStudent

--- src/main/java/Student.java
+++ src/main/java/Student.java
@@ -2,25 +2,12 @@
 
 public class Student {
 
-    int id = 0;
+    int id;
     String first_name;
     String last_name;
     String group_id;
     int coming_year;
 
-    public Student( String first_name, String last_name, String group_id, int coming_year) {
-        this.id++;
-        this.first_name = first_name;
-        this.last_name = last_name;
-        this.group_id = group_id;
-        this.coming_year = coming_year;
-    }
-    public Student( String first_name, String last_name, int coming_year) {
-        this.id++;
-        this.first_name = first_name;
-        this.last_name = last_name;
-        this.coming_year = coming_year;
-    }
     public Student( int id, String first_name, String last_name, int coming_year) {
         this.id = id;
         this.first_name = first_name;
